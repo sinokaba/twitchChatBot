35e144261fcebfffd08aa1fdead9bb7945169758
sinokaba <ronaldal.santos@gmail.com>
2016-07-19 17:07:12 -0400

    Add files via upload

--- twitchbot.js
+++ twitchbot.js
@@ -1,5 +1,5 @@
 var tmi = require('tmi.js');
-var chan = "derogativs";
+var chan = "derogativ";
 var whichChan;
 var options = {
 	options: {
@@ -18,36 +18,41 @@ var options = {
 
 var client = new tmi.client(options);
 var skinGiveaway = ["AWP Dragon Lore", "Karambit doppler", "Ak-47 Vulcun", "M4A1 Masterpiece"];
+var rank = ["Bronze 3", "Silver 1", "Bronze 5", "Silver 4", "Challenger", "Diamond 2", "Diamond 5", "Plat 4", "Plat 1", "Plat 3", "Gold 2", "Gold 3"];
 var giveaway = true;
 var bannedWords = ["noob", "penis", "uninstall", "!drop"];
 var winningNum = 2;
 
 client.connect();
 
-client.api({
+function updateViewerCount(channel){
+	client.api({
 
-    url: 'https://api.twitch.tv/kraken/streams/' + chan 
+	    url: 'https://api.twitch.tv/kraken/streams/' + chan 
 
-}, (err, res, body) => {
+	}, (err, res, body) => {
 
-    // online
+	    // online
 
-    if(body.stream) {
+	    if(body.stream) {
 
-        console.log(body.stream.viewers);
-        if(body.stream.viewers >= 10000){
-        	client.slow(chan, 120);
-        }
-    }
+	        console.log("Current number of viewers: " + body.stream.viewers);
+	        if(body.stream.viewers >= 10000){
+	        	client.slow(chan, 120);
+	        }
+	    }
 
-    // offline
+	    // offline
 
-    else {
-    	console.log("Stream is offline");
-    }
+	    else {
+	    	console.log("Stream is offline");
+	    }
 
-})
+	});
+}
+setInterval(updateViewerCount, 1000 * 15, chan);
 
+function respondToChat(){
 client.on('chat', function(channel, user, message, self){
 	var random = Math.floor(Math.random() * 3);
 
@@ -58,6 +63,9 @@ client.on('chat', function(channel, user, message, self){
 		else if(message == "!clear" && user.username == "derogativ"){
 			client.clear("derogativ");
 		}
+		else if(message == "!bot"){
+			client.action(chan, chan + " is a bot. 4Head");
+		}
 		else if(message == "!startGiveaway" && user.username == "derogativ"){
 			client.action(chan, "Giveaway has started type '!drop' to participate.");
 			giveaway = true;
@@ -66,8 +74,23 @@ client.on('chat', function(channel, user, message, self){
 			client.action("derogativ", "Giveaway has ended.");
 			giveaway = false;
 		}
+		else if(message == "!guessrank"){
+			var ran = Math.floor(Math.random() * rank.length);
+			if(ran == 4 || ran == 5){
+				client.action(chan, user['display-name'] + " Your rank is: " + rank[ran] + " Kappa");
+			}
+			else if(ran == 0 || ran == 2 || ran == 3){
+				client.action(chan, user['display-name'] + " I'm guessing your rank is: " + rank[ran] + " WutFace");				
+			}
+			else{
+				client.action(chan, user['display-name'] + " I'm guessing your rank is: " + rank[ran] + " SoonerLater");
+			}
+		}
+		else if(message == "!giveaway"){
+				client.action(chan, "Items being given away: " + skinGiveaway[0] + ", " + skinGiveaway[1] + ", " + skinGiveaway[2] + ", " + skinGiveaway[3]);
+		}
 		if(giveaway){
-			var userNum = Math.floor(Math.random() * 4);
+			var userNum = Math.floor(Math.random() * 3);
 			bannedWords.splice(2, 3);
 			if(message == "!drop" && userNum == winningNum){
 				//checks whether the skin name starts with a vowel, fixes grammar if so
@@ -85,9 +108,11 @@ client.on('chat', function(channel, user, message, self){
 	}	
 });
 
+}
+
+setInterval(respondToChat, 5000);
 
 client.on('connected', function(address, port){
 	console.log(client.getOptions());
 	console.log("Address: " + address + ". Port: " + port);
-	client.action(chan, "Giveaway has started type '!drop' to participate.");
 });
